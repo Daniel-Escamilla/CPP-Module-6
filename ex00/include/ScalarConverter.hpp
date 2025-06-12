aea5b7df3b5ba8e7e20240c457038dcd5433e879
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-12 11:46:17 +0200

    Refactor ScalarConverter.hpp: reorganize includes and update method signatures for clarity

--- ex00/include/ScalarConverter.hpp
+++ ex00/include/ScalarConverter.hpp
@@ -6,20 +6,19 @@
 /*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 15:42:19 by daniel-esca       #+#    #+#             */
-/*   Updated: 2025/06/12 10:34:49 by daniel-esca      ###   ########.fr       */
+/*   Updated: 2025/06/12 11:23:01 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
-#include <string>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
 #include <limits>
 #include <cmath>
-#include <sstream>
-#include <cmath>
-#include <iostream>
 
 class ScalarConverter
 {
@@ -29,15 +28,17 @@ class ScalarConverter
 
 		static bool parseLiteral(const std::string& str, double& out);
 		static bool hasDecimalPart(double value);
-		static void printWithFormat(double value, bool isFloat);
+		static void printWithFormat(double value, bool isFloat, const std::string& originalStr);
 
 		static bool	isValidNumericLiteral(const std::string& str);
 		static void limits(const std::string& str);
 		
 		static void printChar(int value);
 		static void printInt(double value);
-		static void printFloat(double value);
-		static void printDouble(double value);
+		static void printFloat(double value, const std::string& str);
+		static void printDouble(double value, const std::string& str);
+
+		static int	getDecimalPrecision(const std::string& str);
 
 	public:
 		static void convert(const std::string& literal);
